3aefb935fcd3fa39f028a7b76eca5ab81dd93696
MiguelTolino <mmateo-t@student.42madrid.com>
2022-05-23 00:23:22 +0200

    ex00

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,30 +6,29 @@
 /*   By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 20:17:11 by mmateo-t          #+#    #+#             */
-/*   Updated: 2022/04/25 13:01:46 by mmateo-t         ###   ########.fr       */
+/*   Updated: 2022/05/23 00:22:05 by mmateo-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
-Fixed::Fixed()
+Fixed::Fixed() : _integer(0)
 {
-	this->integer = 0;
 	std::cout << "Default constructor called\n";
 }
 
-Fixed& Fixed::operator= (const Fixed& number) {
-
-	this->integer = number.integer;
+Fixed & Fixed::operator= (const Fixed &number)
+{
 	std::cout << "Assignation operator called\n";
-	return *this;
+	this->_integer = number.getRawBits();
+	return (*this);
 }
 
-Fixed::Fixed (const Fixed& number) {
-
-	this->integer = number.integer;
+Fixed::Fixed(const Fixed &number)
+{
 	std::cout << "Copy constructor called\n";
+	*this = number;
 }
 
 Fixed::~Fixed()
@@ -38,13 +37,14 @@ Fixed::~Fixed()
 	return;
 }
 
-void Fixed::setRawBits ( int const raw) {
-
-	this->integer = raw;
+void Fixed::setRawBits(int const raw)
+{
+	this->_integer = raw;
 }
 
-int Fixed::getRawBits ( void ) const {
+int Fixed::getRawBits(void) const
+{
 
 	std::cout << "getRawBits member function called\n";
-	return this->integer;
+	return this->_integer;
 }
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,20 +6,25 @@
 /*   By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 20:17:02 by mmateo-t          #+#    #+#             */
-/*   Updated: 2022/04/25 13:02:03 by mmateo-t         ###   ########.fr       */
+/*   Updated: 2022/05/23 00:22:53 by mmateo-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
 class Fixed
 {
-private:
-	int integer;
-	static const int fract_bits = 8;
-public:
-	Fixed();
-	Fixed(const Fixed& );
-	Fixed& operator= (const Fixed& );
-	~Fixed();
-	int getRawBits( void ) const;
-	void setRawBits ( int const );
+	private:
+		int _integer;
+		static const int _fractBits = 8;
+	public:
+		Fixed( void );
+		Fixed(const Fixed& );
+		Fixed & operator= (const Fixed & );
+		~Fixed();
+		int getRawBits( void ) const;
+		void setRawBits ( int const );
 };
+
+#endif
